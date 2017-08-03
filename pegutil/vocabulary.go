eb095db3e767dede053e2225120d2748c0f1d863
hucsmn <hucsmn@hotmail.com>
2017-08-03 19:24:15 +0800

    add: pegutil vocabulary

---
+++ pegutil/vocabulary.go
@@ -0,0 +1,91 @@
+package pegutil
+
+import (
+	"github.com/hucsmn/peg"
+)
+
+// Vocabulary is all the variables defined in this package.
+var Vocabulary = map[string]peg.Pattern{
+	"OctDigit": OctDigit,
+	"DecDigit": DecDigit,
+	"HexDigit": HexDigit,
+
+	"ASCIIWhitespace":    ASCIIWhitespace,
+	"ASCIINotWhitespace": ASCIINotWhitespace,
+	"ASCIIDigit":         ASCIIDigit,
+	"ASCIILetter":        ASCIILetter,
+	"ASCIILower":         ASCIILower,
+	"ASCIIUpper":         ASCIIUpper,
+	"ASCIILetterDigit":   ASCIILetterDigit,
+	"ASCIIControl":       ASCIIControl,
+	"ASCIINotControl":    ASCIINotControl,
+
+	"Whitespace":    Whitespace,
+	"NotWhitespace": NotWhitespace,
+	"Digit":         Digit,
+	"Letter":        Letter,
+	"Lower":         Lower,
+	"Upper":         Upper,
+	"Title":         Title,
+	"LetterDigit":   LetterDigit,
+	"Control":       Control,
+	"NotControl":    NotControl,
+	"Printable":     Printable,
+	"NotPrintable":  NotPrintable,
+	"Graphic":       Graphic,
+	"NotGraphic":    NotGraphic,
+
+	"NewlineRune":    NewlineRune,
+	"NotNewlineRune": NotNewlineRune,
+
+	"DecInteger": DecInteger,
+	"DecUint8":   DecUint8,
+	"DecUint16":  DecUint16,
+	"DecUint32":  DecUint32,
+	"DecUint64":  DecUint64,
+
+	"HexInteger": HexInteger,
+	"HexUint8":   HexUint8,
+	"HexUint16":  HexUint16,
+	"HexUint32":  HexUint32,
+	"HexUint64":  HexUint64,
+
+	"OctInteger": OctInteger,
+	"OctUint8":   OctUint8,
+	"OctUint16":  OctUint16,
+	"OctUint32":  OctUint32,
+	"OctUint64":  OctUint64,
+
+	"SimpleHexUint8":  SimpleHexUint8,
+	"SimpleHexUint16": SimpleHexUint16,
+	"SimpleHexUint32": SimpleHexUint32,
+	"SimpleHexUint64": SimpleHexUint64,
+
+	"Integer":    Integer,
+	"Decimal":    Decimal,
+	"Float":      Float,
+	"Number":     Number,
+	"Identifier": Identifier,
+	"AnySpaces":  AnySpaces,
+	"Spaces":     Spaces,
+	"Newline":    Newline,
+	"String":     String,
+
+	"MAC":          MAC,
+	"EUI64":        EUI64,
+	"IP":           IP,
+	"CIDR":         CIDR,
+	"IPv4":         IPv4,
+	"CIDRv4":       CIDRv4,
+	"IPv6":         IPv6,
+	"CIDRv6":       CIDRv6,
+	"IPv6WithZone": IPv6WithZone,
+
+	"URI":          URI,
+	"URIHost":      URIHost,
+	"URIAbsolute":  URIAbsolute,
+	"URIReference": URIReference,
+	"Slug":         Slug,
+	"Domain":       Domain,
+	"EMail":        EMail,
+}
